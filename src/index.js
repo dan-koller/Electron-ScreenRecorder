fe0ed1dbe37c567ab341d2de028f368aa042114a
Dan Koller <57103678+dankoller112@users.noreply.github.com>
2021-03-16 19:38:48 +0100

    Enabled remote media to make app work in electron v9

--- src/index.js
+++ src/index.js
@@ -9,10 +9,12 @@ if (require('electron-squirrel-startup')) { // eslint-disable-line global-requir
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1000,
+    height: 800,
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true,
     }
   });
 
